0acf413f8d28c544f03ba93b718f0c8a747c1bca
yeghishehov <yeghishehov.1@gmail.com>
2020-09-03 14:37:09 +0400

    feat(App) import Main

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import { ThemeProvider } from '@material-ui/core/styles';
+import Main from './pages/main/Main';
 
 function App() {
   return (
-    <div>
-      HOME
-    </div>
+    <Router>
+      <Main />
+    </Router>
   );
 }
 
