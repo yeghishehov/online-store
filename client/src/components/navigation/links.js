78ddfe66d49d02df523df88a0b4303ebd1a18d86
yeghishehov <yeghishehov.1@gmail.com>
2020-09-03 14:38:15 +0400

    feat(links) array of links

---
+++ client/src/components/navigation/links.js
@@ -0,0 +1,10 @@
+import { ROUTES } from '../../globals/routes';
+
+export const links = [
+  { route: ROUTES.home, text: "Home" },
+  { route: ROUTES.men, text: "Men" },
+  { route: ROUTES.women, text: "Women" },
+  { route: ROUTES.outlet, text: "Outlet" },
+  { route: ROUTES.about, text: "About" },
+  { route: ROUTES.contact, text: "Contact" },
+];
