56f48170c9fa181962cc1866af20d8d72d7beb72
yeghishehov <yeghishehov.1@gmail.com>
2020-09-05 01:10:52 +0400

    create account

---
+++ client/src/account/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import useStyles from './style'
+
+export default function Login () {
+  const classes = useStyles();
+
+  return (
+    <div>
+    Login
+    </div>
+  )
+}
---
+++ client/src/account/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import useStyles from './style'
+
+export default function Register () {
+  const classes = useStyles();
+
+  return (
+    <div>
+    Register
+    </div>
+  )
+}
