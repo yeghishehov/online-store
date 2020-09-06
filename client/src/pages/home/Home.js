c5f004e2d38a0c074b96ea7c21cb3b1f141ce529
yeghishehov <yeghishehov.1@gmail.com>
2020-09-06 15:04:31 +0400

    feat(Home) add Collections

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import Link from '@material-ui/core/Link'
+import { Link } from 'react-router-dom'
 import Typography from '@material-ui/core/Typography';
 import Slider from './slider/Slider'
 import Collections from './collections/Collections'
+import { ROUTES } from '../../globals/routes'
 import useStyles from './home.style'
 
 export default function Home () {
@@ -11,12 +12,35 @@ export default function Home () {
   return (
     <div className={classes.root}>
       <Slider />
+
       <Collections 
         title='Flat Rate' 
         text='Our best sellers at 45€' 
-        route='/collections/flat-rate-46'
-        viewItems
+        route={ROUTES.flatRate46}
+        viewItems={[0,1,2,3,4,5,6,7,8,1,2,2,2,2,2]}
       />
+      <Collections 
+        title='Summer special edition' 
+        text='-15% light and breathable models.' 
+        route={ROUTES.summerSpecialSelection}
+        viewItems={[0,1,2,3,4,5,6,7,8,1,2,2,2,2,2]}
+      />
+      <Collections 
+        title='Outlet' 
+        text='Outlet > Our greatest hits at the best price' 
+        route={ROUTES.outlet}
+        viewItems={[0,1,2,3,4,5,6,7,8,1,2,2,2,2,2]}
+      />
+
+      <div className={classes.collectionManWoman}>
+        <Link to={ROUTES.men} className={classes.link} >
+          Men
+        </Link>
+        <div className={classes.space}></div>
+        <Link to={ROUTES.women} className={classes.link} >
+          Women
+        </Link>
+      </div>
     </div>
   )
 }
--- client/src/pages/home/home.style.js
+++ client/src/pages/home/home.style.js
@@ -11,5 +11,29 @@ export default makeStyles((theme) => ({
   typography: {
 
   },
-
+  collectionManWoman: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100vw',
+    height: 700,
+    background: `url(${"https://cdn.shopify.com/s/files/1/1241/4530/files/nuevo_chica_1.jpg"}) 100% 100% no-repeat`,
+    backgroundSize: '100%',
+  },
+  link: {
+    textDecoration: 'none',
+    paddingTop: 200,
+    fontSize: 24,
+    fontWeight: 500,
+    color: '#fff',
+    borderBottom: '1px solid #fff',
+    '&:hover': {
+      borderBottom: '1px solid #bbb',
+      color: '#bbb'
+    },
+  },
+  space: {
+    width: 100,
+    height: 100,
+  }
 }));
