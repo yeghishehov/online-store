36e9e67524857751c2adf9e4b787efb748edda57
yeghishehov <yeghishehov.1@gmail.com>
2020-09-06 15:03:57 +0400

    feat(Collections) change marginBottom

--- client/src/pages/home/collections/collections.style.js
+++ client/src/pages/home/collections/collections.style.js
@@ -4,7 +4,7 @@ export default makeStyles((theme) => ({
   root: {
     // display: 'flex',
     // justifyContent: 'space-between',
-    marginBottom: 10,
+    marginBottom: 40,
     [theme.breakpoints.down('sm')]: {
       
     },
