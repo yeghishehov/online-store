18768326fe89f6955cbc6b899668792a45461c12
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-09-27 01:55:55 +0400

    feat(Login) created registration logic

--- client/src/account/Login.js
+++ client/src/account/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import { Typography, TextField, Snackbar } from '@material-ui/core';
-import MuiAlert  from '@material-ui/lab/Alert';
+import { Typography, TextField } from '@material-ui/core';
 import axios from '../utils/axiosConfig';
 import useStyles from './style'
 
@@ -10,55 +9,66 @@ export default function Login () {
   ////------------------------- registration testing-------------------------------- /////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   const [form, setForm] = useState({email: '', password: ''});
-  const [error, setError] = useState("");
+  const [error, setError] = useState({email: "", password: "", message: ""});
+  const [message, setMessage] = useState('');
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   }
   
   const registerHandler = async () => {
+    setError({ email: "", password: "", message: "" });
+    setMessage("")
     try {
-      const data = await axios.post(`/api/auth/register`, {...form})
-
+      const request = await axios.post(`/api/auth/register`, {...form})
+      setMessage(request.data.message)
+      setForm({email: '', password: ''});
     } catch (e) {
-      setError(e.message)
+      const data = e.response.data;
+      const isErroresExisted = 'errors' in data
+      if (isErroresExisted){
+        const email = data.errors.find(err => err.param === 'email')
+        const pass = data.errors.find(err => err.param === 'password')
+        console.log(email + " " + pass)
+        setError({ 
+          email: email ? email.msg : "",
+          password: pass ? pass.msg : "",
+          message: data.message,
+        })
+      } else {
+        setError({ ...error, message: data.message })
+      }
     }
   }
 
   return (
     <div className={classes.root} >
-      {error 
-        ? (
-          <Snackbar >
-            <MuiAlert variant="filled" severity="error" >
-              {`${error}`}
-            </MuiAlert>
-          </Snackbar>
-        ) : null
-      }
       <Typography align='center' variant='h3' >
         Login
       </Typography>
 
       <div className={classes.input}>
         <div className={classes.textField}>
-          Email
+          Email 
           <TextField
             name = 'email'
             value = {form.email}
             onChange = {handleChange}
             variant = "outlined"
           />
+          {error.email}
         </div>
         <div className={classes.textField}>
-          Password
+          Password 
           <TextField
             name = 'password'
             value = {form.password}
             onChange = {handleChange}
             variant = "outlined"
           />
+          {error.password}
         </div>
+        { `${error.message}${message}` }
       </div>
       <button onClick={registerHandler} >reg</button>
     </div>
