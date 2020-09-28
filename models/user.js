9c7cc7210b4267d4237e2372e20f039efc83f68f
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-09-28 16:03:01 +0400

    feat(user) added first and lasn names

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ const {Schema, model, Types} = require('mongoose')
 const schema = new Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
     links: [{type: Types.ObjectId, ref: 'Link'}]
 })
 
