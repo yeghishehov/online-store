210f029911390ba9e533c52fa9c26ba67419ec8e
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-15 20:41:53 +0400

    stylr(model/shoes) collection renamed

--- models/shoes.js
+++ models/shoes.js
@@ -2,11 +2,12 @@ const {Schema, model} = require('mongoose')
 
 const media = new Schema({
     color: {type: String, required: true},
-    imagesSrc: [String]
+    imagesSrc: [String],
 })
 const schema = new Schema({
     name: {type: String, required: true},
     type: {type: String, required: true},
+    collection_name: {type: String, required: true},
     price: {type: String, required: true},
     sizes: [String],
     description: {type: String, required: true},
