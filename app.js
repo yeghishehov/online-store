31f13faf8ba6a0a2d429a13963511a487a3cf56a
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-15 17:14:42 +0400

    feat(app) added app/shoes

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
 
+app.use('/app/shoes', require('./routes/shoes.routers'))
+
 const PORT = config.get('port') || 5000
 
 async function start() {
