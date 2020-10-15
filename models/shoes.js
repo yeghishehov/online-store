9818e238bc9eb93e6af11d9d6320c645d8a2705f
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-15 20:19:21 +0400

    feat(models/shoes) add media, type, sizes options

--- models/shoes.js
+++ models/shoes.js
@@ -1,10 +1,16 @@
 const {Schema, model} = require('mongoose')
 
+const media = new Schema({
+    color: {type: String, required: true},
+    imagesSrc: [String]
+})
 const schema = new Schema({
-    name: {type: String, required: true, unique: true},
+    name: {type: String, required: true},
+    type: {type: String, required: true},
     price: {type: String, required: true},
+    sizes: [String],
     description: {type: String, required: true},
-    images: [String],
+    media: [media],
 })
 
 module.exports = model('Shoes', schema)
