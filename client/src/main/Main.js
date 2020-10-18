cfe4836ecded549abaa791b16d28c0a6dd17cb52
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-18 13:03:10 +0400

    style(Main) routes corrected

--- client/src/main/Main.js
+++ client/src/main/Main.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Header from '../components/header/Header'
-import Footer from '../components/footer/Footer'
-import { useRoutes } from '../routes';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+import routes from '../routes';
 
 export default function Main() {
-  const routes = useRoutes();
-
   return (
     <div>
       <Header />
-      {routes}      
+      {routes()}
       <Footer />
     </div>
-  )
+  );
 }
