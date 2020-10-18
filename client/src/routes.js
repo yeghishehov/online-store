f3b14a493482c738b3f7c1ab6e73b82add778b8f
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-18 12:58:50 +0400

    ref(client/routes) routes adjusted

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,166 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import {Switch, Route} from 'react-router-dom';
-import { ROUTES } from './globals/routes';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import ROUTES from './globals/routes';
 
-import Home from './pages/home/Home'
-import Men from './collections/men/Men'
-import Women from './collections/women/Women'
-import Outlet from './collections/outlet/Outlet'
-import Shipment from './pages/shipment/Shipment'
-import Exchanges from './pages/exchanges/Exchanges'
-import Returns from './pages/returns/Returns'
-import SizeChart from './pages/sizeChart/SizeChart'
-import Faq from './pages/faq/Faq'
-import About from './pages/about/About'
-import Contact from './pages/contact/Contact'
-import DressYourProject from './pages/dressYourProject/DressYourProject'
-import Design from './pages/design/Design'
-import OfficialStores from './pages/officialStores/OfficialStores'
-import Legal from './pages/legal/Legal'
-import Login from './account/Login'
-import Register from './account/Register'
-import AuthPage from './pages/AuthPage'; 
-import axios from './utils/axiosConfig';
+import Home from './pages/home/Home';
+import Men from './collections/men/Men';
+import Women from './collections/women/Women';
+import Outlet from './collections/outlet/Outlet';
+import Shipment from './pages/shipment/Shipment';
+import Exchanges from './pages/exchanges/Exchanges';
+import Returns from './pages/returns/Returns';
+import SizeChart from './pages/sizeChart/SizeChart';
+import Faq from './pages/faq/Faq';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import DressYourProject from './pages/dressYourProject/DressYourProject';
+import Design from './pages/design/Design';
+import OfficialStores from './pages/officialStores/OfficialStores';
+import Legal from './pages/legal/Legal';
+import Login from './account/Login';
+import Register from './account/Register';
+import AuthPage from './pages/AuthPage';
 
-export const useRoutes = () => {
-    const [isAuthorized, setIsAuthorized] = useState(false)
+export default () => (
+  <Switch>
+    <Route exact path={ROUTES.home} component={Home} />
+    <Route path={ROUTES.men} component={Men} />
+    <Route path={ROUTES.women} component={Women} />
+    <Route path={ROUTES.outlet} component={Outlet} />
 
-    const checkingAuthorization = async () => {
-        const data = JSON.parse(localStorage.getItem('userData'));
-        if(data && data.token) {
-            try {
-                const aaa = await axios.post('/api/auth/isAuthorized', { token: data.token})
-                setIsAuthorized(aaa.data)
-            } catch (error) {
-                console.log( error.response.data )
-                setIsAuthorized(false)
-                localStorage.removeItem('userData')
-            }
-        }
-    }
+    <Route path={ROUTES.shipment} component={Shipment} />
+    <Route path={ROUTES.exchanges} component={Exchanges} />
+    <Route path={ROUTES.sizeChart} component={SizeChart} />
+    <Route path={ROUTES.faq} component={Faq} />
+    <Route path={ROUTES.returns} component={Returns} />
 
-    useEffect(() => {
-        checkingAuthorization()
-    }, [])
+    <Route path={ROUTES.dressYourProject} component={DressYourProject} />
+    <Route path={ROUTES.design} component={Design} />
+    <Route path={ROUTES.officialStores} component={OfficialStores} />
+    <Route path={ROUTES.legal} component={Legal} />
+    <Route path={ROUTES.about} component={About} />
+    <Route path={ROUTES.contact} component={Contact} />
 
-
-
-    if (isAuthorized) {
-        return(
-            <Switch>
-            <Route exact path={ROUTES.home}>
-                <Home />
-            </Route>
-            <Route path={ROUTES.men}>
-                <Men />
-            </Route>
-            <Route path={ROUTES.women}>
-                <Women />
-            </Route>
-            <Route path={ROUTES.outlet}>
-                <Outlet />
-            </Route>
-
-
-            <Route path={ROUTES.shipment}>
-                <Shipment />
-            </Route>
-            <Route path={ROUTES.exchanges}>
-                <Exchanges />
-            </Route>
-            <Route path={ROUTES.sizeChart}>
-                <SizeChart />
-            </Route>
-            <Route path={ROUTES.faq}>
-                <Faq />
-            </Route>
-            <Route path={ROUTES.returns}>
-                <Returns />
-            </Route>
-
-            <Route path={ROUTES.dressYourProject}>
-                <DressYourProject />
-            </Route>
-            <Route path={ROUTES.design}>
-                <Design />
-            </Route>
-            <Route path={ROUTES.officialStores}>
-                <OfficialStores />
-            </Route>
-            <Route path={ROUTES.legal}>
-                <Legal />
-            </Route>
-            <Route path={ROUTES.about}>
-                <About />
-            </Route>
-            <Route path={ROUTES.contact}>
-                <Contact />
-            </Route>
-
-            <Route path={ROUTES.authPage}>
-                <AuthPage />
-            </Route>
-        </Switch>
-        )
-    }
-
-    return(
-        <Switch>
-            <Route exact path={ROUTES.home}>
-                <Home />
-            </Route>
-            <Route path={ROUTES.men}>
-                <Men />
-            </Route>
-            <Route path={ROUTES.women}>
-                <Women />
-            </Route>
-            <Route path={ROUTES.outlet}>
-                <Outlet />
-            </Route>
-
-
-            <Route path={ROUTES.shipment}>
-                <Shipment />
-            </Route>
-            <Route path={ROUTES.exchanges}>
-                <Exchanges />
-            </Route>
-            <Route path={ROUTES.sizeChart}>
-                <SizeChart />
-            </Route>
-            <Route path={ROUTES.faq}>
-                <Faq />
-            </Route>
-            <Route path={ROUTES.returns}>
-                <Returns />
-            </Route>
-
-            <Route path={ROUTES.dressYourProject}>
-                <DressYourProject />
-            </Route>
-            <Route path={ROUTES.design}>
-                <Design />
-            </Route>
-            <Route path={ROUTES.officialStores}>
-                <OfficialStores />
-            </Route>
-            <Route path={ROUTES.legal}>
-                <Legal />
-            </Route>
-            <Route path={ROUTES.about}>
-                <About />
-            </Route>
-            <Route path={ROUTES.contact}>
-                <Contact />
-            </Route>
-
-            <Route path={ROUTES.login}>
-                <Login />
-            </Route>
-            <Route path={ROUTES.register}>
-                <Register />
-            </Route>
-        </Switch>
-    )
-}
+    <Route path={ROUTES.login} component={Login} />
+    <Route path={ROUTES.register} component={Register} />
+    <Route path={ROUTES.authPage} component={AuthPage} />
+  </Switch>
+);
