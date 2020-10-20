a89f48b7d9ca0a06db6ae26ed49cea5333ce4fa3
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-20 12:00:46 +0400

    ref(client/authHook) userId changed to name

--- client/src/hooks/authHook.js
+++ client/src/hooks/authHook.js
@@ -1,46 +1,49 @@
 import { useState, useCallback, useEffect } from 'react';
+import axios from '../utils/axiosConfig';
 import { checkAuthorization } from '../API/authApi';
 
 const storageName = 'userData';
 
 export default () => {
   const [token, setToken] = useState(null);
-  const [userId, setUserId] = useState(null);
+  const [name, setName] = useState(null);
   const [isAuthorized, setIsAuthorized] = useState(false);
 
-  const login = useCallback((jwtToken, id) => {
+  const login = useCallback((jwtToken, userName) => {
     setToken(jwtToken);
-    setUserId(id);
+    setName(userName);
     setIsAuthorized(true);
+    axios.setXApKey(jwtToken);
 
     localStorage.setItem(storageName, JSON.stringify({
-      token: jwtToken, userId: id,
+      token: jwtToken, name: userName,
     }));
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
-    setUserId(null);
+    setName(null);
     setIsAuthorized(false);
     localStorage.removeItem(storageName);
   }, []);
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem(storageName));
-    if (data && data.token) {
+    if (data && data.token && data.name) {
+      axios.setXApKey(data.token);
       const checkingAuthorization = async () => {
         try {
           await checkAuthorization(data.token);
-          login(data.token, data.userId);
+          login(data.token, data.name);
         } catch (error) {
           logout();
         }
       };
       checkingAuthorization();
-    }
+    } else logout();
   }, [login, logout]);
 
   return {
-    login, logout, token, userId, isAuthorized, setIsAuthorized,
+    login, logout, token, name, isAuthorized, setIsAuthorized,
   };
 };
