621864a2e2242d046b962a4b7403df68593c6648
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-20 12:03:16 +0400

    feat(client/AuthPage) add file submit

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useAuthContext } from '../contexts/authContext';
 import ROUTES from '../globals/routes';
+import axios from '../utils/axiosConfig';
 
 export default function AuthPage() {
-  const { logout, isAuthorized } = useAuthContext();
+  const { logout, isAuthorized, name } = useAuthContext();
+  const [files, setFiles] = useState(null);
 
   const handleLogout = () => {
     logout();
+  };
+
+  const handleSelectFile = (e) => {
+    setFiles(e.target.files);
+  };
+
+  const handleSubmitFile = () => {
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i += 1) {
+      formData.append('image', files[i]);
+    }
+    axios.post('/api/uploadfile', formData);
   };
 
   if (!isAuthorized) return <Redirect to={ROUTES.login} />;
@@ -15,9 +29,15 @@ export default function AuthPage() {
   return (
     <>
       <h1>
-        USER PAGE
+        {name}
       </h1>
       <button type="button" onClick={handleLogout}>Logout</button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <input type="file" multiple onChange={handleSelectFile} />
+      <button type="submit" onClick={handleSubmitFile}>Load</button>
     </>
   );
 }
