f35531f8cec848636ccf296e65761cf6ff02d52a
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-20 11:58:41 +0400

    feat(app) used bodyParser, passport, morgan, cors

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const config = require('config')
 const mongoose = require('mongoose')
+const passport = require('passport')
+
+const upload = require('./middleware/upload')
 
 const app = express()
 
-app.use(express.json({ extended: true }))
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
+
+app.use(require('morgan')('dev'))
+app.use(require('cors')())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/app/shoes', require('./routes/shoes.routers'))
 app.use('/api/collections', require('./routes/collections.routes'))
+app.use('/api/uploadfile', passport.authenticate('jwt', {session: false}), upload.array('image'), async (req, res) => {
+    console.log(req.files)
+    res.json('ok')
+})
 
 const PORT = config.get('port') || 5000
 
