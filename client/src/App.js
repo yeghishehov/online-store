c604c0a73484322677c9da494c33e6e59c74c7aa
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-23 15:45:39 +0400

    feat(clinet/App) added redux provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AuthProvider from './providers/AuthProvider';
+import store from './store';
 import Main from './components/main/Main';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Main />
-      </Router>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <Router>
+          <Main />
+        </Router>
+      </AuthProvider>
+    </Provider>
   );
 }
 
