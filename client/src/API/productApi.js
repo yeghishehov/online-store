a3b0fe27a77bee7c222241e216c7fbd8a7af0226
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-23 17:18:53 +0400

    feat(client/productAPI) created all apis

--- client/src/API/productApi.js
+++ client/src/API/productApi.js
@@ -1,5 +1,17 @@
 import axios from '../utils/axiosConfig';
 
-export const getAllProducts = () => (
+export const getAllRequest = () => (
   axios.get('/shoes/all')
+);
+
+export const getMenRequest = () => (
+  axios.get('/collections/men')
+);
+
+export const getWomenRequest = () => (
+  axios.get('/collections/women')
+);
+
+export const getOutletRequest = () => (
+  axios.get('/collections/outlet')
 );
