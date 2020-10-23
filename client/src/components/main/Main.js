fdd4d05f60c60d5b5068be6dfbce501003329da6
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-23 15:47:05 +0400

    feat(client/Main) added dispatch products

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import getProducts from '../../store/actions/products';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import routes from '../../routes';
 
 export default function Main() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+
   return (
     <div>
       <Header />
