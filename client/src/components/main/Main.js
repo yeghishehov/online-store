14755d1c25e23a2a9408286f16a7df8e79f1e351
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-10-23 17:26:10 +0400

    ref(client/Main) removed unnecessary imports

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import getProducts from '../../store/actions/products';
+import React from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import routes from '../../routes';
 
 export default function Main() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getProducts());
-  }, []);
-
   return (
     <div>
       <Header />
