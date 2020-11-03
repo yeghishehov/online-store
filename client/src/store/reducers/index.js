86da2eb1b0fdfc2d5ef9caed41765249580c4e89
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-11-04 02:32:45 +0400

    feat(client/store) add order reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import productsReducer from './productsReducer';
+import ordersReducer from './ordersReducer';
 
 export default combineReducers({
   user: userReducer,
   products: productsReducer,
+  orders: ordersReducer,
 });
