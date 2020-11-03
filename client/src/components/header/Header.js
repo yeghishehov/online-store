c5005d9ea1a9b0b13aefe91b4f55b0567b0a1e4b
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-11-04 02:29:31 +0400

    feat(client/Header) added user name

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
@@ -10,7 +11,8 @@ import ROUTES from '../../globals/routes';
 
 export default function Header() {
   const classes = useStyles();
-
+  const { data } = useSelector((state) => state.orders);
+  const { data: user } = useSelector((state) => state.user);
   return (
     <div className={classes.root}>
       <div className={classes.nav}>
@@ -23,10 +25,19 @@ export default function Header() {
 
       <div className={classes.icons}>
         <SearchIcon fontSize="inherit" />
-        <Link to={ROUTES.authPage}>
+        <Link to={ROUTES.authPage} className={classes.link}>
           <PermIdentityIcon fontSize="inherit" />
+          <div className={classes.name}>
+            {user.firstName}
+          </div>
         </Link>
-        <ShoppingCartIcon fontSize="inherit" />
+        <Link to="#" className={classes.link}>
+          <ShoppingCartIcon fontSize="inherit" />
+          {data.length > 0
+            ? <span className={classes.ordersCounts}>{data.length}</span>
+            : null
+          }          
+        </Link>
       </div>
     </div>
   );
