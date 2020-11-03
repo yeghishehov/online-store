e1c24f03b025e3c3632f4bf37a299ba653346735
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-11-04 02:16:05 +0400

    feat(app) use orders

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use('/images',express.static('images'));
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/shoes', require('./routes/shoes.routers'))
 app.use('/api/collections', require('./routes/collections.routes'))
+app.use('/api/orders', require('./routes/orders.routes'))
 
 const PORT = config.get('port') || 5000
 
