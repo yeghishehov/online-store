75bc441e3acade753a7038ca31f0ed3584998620
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-11-04 02:26:54 +0400

    feat(clinet/Main) dispatch getOrder

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getOrder } from '../../store/actions/orders'
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import routes from '../../routes';
 
 export default function Main() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getOrder())
+  })
+
   return (
     <div>
       <Header />
