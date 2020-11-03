966d38c8a009b5a3987f6791eec55993d85ac3ff
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-11-04 02:24:13 +0400

    feat(client/store) finifed creating user

--- client/src/store/actions/user.js
+++ client/src/store/actions/user.js
@@ -1,15 +1,10 @@
-import { GET_USER, GET_USER_SUCCESS, GET_USER_FAILURE } from './constants';
+import { GET_USER_SUCCESS } from './constants';
 
-export const getUserLoading = () => ({
-  type: GET_USER,
-});
-
-export const getUserSuccess = (payload) => ({
+const getUserSuccess = (payload) => ({
   type: GET_USER_SUCCESS,
   payload,
 });
 
-export const getUserFailure = (error) => ({
-  type: GET_USER_FAILURE,
-  error,
-});
+export default (user) => async (dispatch) => {
+  dispatch(getUserSuccess(user))
+}
--- client/src/store/reducers/userReducer.js
+++ client/src/store/reducers/userReducer.js
@@ -1,25 +1,25 @@
-import { GET_PRODUCTS, GET_PRODUCTS_SUCCESS, GET_PRODUCTS_FAILURE } from '../actions/constants';
+import { GET_USER, GET_USER_SUCCESS, GET_USER_FAILURE } from '../actions/constants';
 
 const initialState = {
-  data: [],
+  data: {},
   loading: false,
   error: '',
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case GET_PRODUCTS:
+    case GET_USER:
       return {
         ...state,
         loading: true,
       };
-    case GET_PRODUCTS_SUCCESS:
+    case GET_USER_SUCCESS:
       return {
         data: action.payload,
         loading: false,
         error: '',
       };
-    case GET_PRODUCTS_FAILURE:
+    case GET_USER_FAILURE:
       return {
         data: [],
         loading: false,
