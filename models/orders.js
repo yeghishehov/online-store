5fea7bb3ed65d400e7b257cba2950be02679b40c
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-11-04 14:50:48 +0400

    feat(models) in order added userId

--- models/orders.js
+++ models/orders.js
@@ -1,12 +1,13 @@
 const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
+    userId: {type: String, required: true},
     collectionName: {type: String, required: true},
     type: {type: String, required: true},
     shoesName: {type: String, required: true},
     color: {type: String, required: true},
     price: {type: String, required: true},
-    size: {type: String, required: true},
+    size: {type: String},
     imagesSrc: {type: String, required: true},
     productPageSrc: {type: String, required: true},
 })
