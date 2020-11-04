d1f855d97ec13c17ce32be4ed0bac440890b4c7b
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2020-11-04 14:51:31 +0400

    feat(order.routes)  added pasport middlware

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const ordersController = require('../controllers/orders.controller')
+const passport = require('passport')
 
 // /api/orders/
-router.get('/getOrders', ordersController.getOrders)
-router.post('/create', ordersController.create)
-router.delete('/remove:id', ordersController.remove)
+router.get('/getOrders', passport.authenticate('jwt', {session: false}), ordersController.getOrders)
+router.post('/create', passport.authenticate('jwt', {session: false}), ordersController.create)
+router.delete('/remove:id', passport.authenticate('jwt', {session: false}), ordersController.remove)
 
 module.exports = router
